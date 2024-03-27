f100fed37ae749b317d3aca0c2b37853ba1406b0
GlenALarson <markkula.antti@gmail.com>
2024-03-27 14:27:56 +0200

    Yksinkertaistettu toimintalogiikkaa

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './App.css';
-
 
 // API keyt haetaan .envistä
 const AUTH_TOKEN = process.env.REACT_APP_READ_ACCESS_TOKEN;
@@ -10,44 +9,7 @@ function App() {
   const [searchQuery, setSearchQuery] = useState('');
   const [movies, setMovies] = useState([]);
   const [selectedMovieIndex, setSelectedMovieIndex] = useState(0);
-  const [posterUrl, setPosterUrl] = useState('');
 
-  // Haetaan tämänhetkisen leffan posteri
-  useEffect(() => {
-    const fetchPoster = async () => {
-      try {
-        if (movies.length > 0) {
-          const movieId = movies[selectedMovieIndex].id;
-          const options = {
-            method: 'GET',
-            headers: {
-              accept: 'application/json',
-              Authorization: 'Bearer ' + AUTH_TOKEN
-            }
-          };
-
-          const response = await fetch(
-            `https://api.themoviedb.org/3/movie/${movieId}/images?api_key=${API_KEY}`,
-            options
-          );
-          const data = await response.json();
-
-          if (data.posters && data.posters.length > 0) {
-            const posterPath = data.posters[0].file_path;
-            setPosterUrl(`https://image.tmdb.org/t/p/original${posterPath}`);
-          } else {
-            setPosterUrl('');
-          }
-        }
-      } catch (error) {
-        console.error('Error fetching poster:', error);
-      }
-    };
-
-    fetchPoster();
-  }, [selectedMovieIndex, movies]);
-
-  // Kun käyttäjä klikkaa 'Search', tehdään haku syötetyillä hakusanoilla TMDB API:in ja valitaan defaulttina ensimmäinen leffa listasta joka sieltä palautuu
   const handleSearch = async () => {
     try {
       const options = {
@@ -59,14 +21,22 @@ function App() {
       };
 
       const response = await fetch(
-        `https://api.themoviedb.org/3/search/movie?query=${searchQuery}&include_adult=false&language=en-US&page=1`,
+        `https://api.themoviedb.org/3/search/movie?query=${searchQuery}&include_adult=false&language=en-US&page=1&api_key=${API_KEY}`,
         options
       );
 
       const data = await response.json();
 
       if (data.results && data.results.length > 0) {
-        setMovies(data.results);
+        const updatedMovies = data.results.map(movie => ({
+          id: movie.id,
+          title: movie.title,
+          overview: movie.overview,
+          release_date: movie.release_date,
+          vote_average: movie.vote_average,
+          poster_path: `https://image.tmdb.org/t/p/original${movie.poster_path}`
+        }));
+        setMovies(updatedMovies);
         setSelectedMovieIndex(0);
       } else {
         setMovies([]);
@@ -77,13 +47,12 @@ function App() {
     }
   };
 
-  // Buttoneilla voi selata hakutuloksia
   const handlePrevMovie = () => {
-    setSelectedMovieIndex((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : prevIndex));
+    setSelectedMovieIndex(prevIndex => (prevIndex > 0 ? prevIndex - 1 : prevIndex));
   };
 
   const handleNextMovie = () => {
-    setSelectedMovieIndex((prevIndex) => (prevIndex < movies.length - 1 ? prevIndex + 1 : prevIndex));
+    setSelectedMovieIndex(prevIndex => (prevIndex < movies.length - 1 ? prevIndex + 1 : prevIndex));
   };
 
   return (
@@ -94,17 +63,23 @@ function App() {
           type="text"
           placeholder="Search for a movie..."
           value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
+          onChange={e => setSearchQuery(e.target.value)}
         />
         <button onClick={handleSearch}>Search</button>
       </div>
       {movies.length > 0 && (
         <div className="movie-info">
-          <h2>{movies[selectedMovieIndex].original_title}</h2>
+          <h2>{movies[selectedMovieIndex].title}</h2>
           <p>{movies[selectedMovieIndex].overview}</p>
           <p>Release Date: {movies[selectedMovieIndex].release_date}</p>
           <p>Vote Average: {movies[selectedMovieIndex].vote_average}</p>
-          {posterUrl && <img src={posterUrl} alt="Movie Poster" style={{ height: '500px', width: 'auto' }}/>}
+          {movies[selectedMovieIndex].poster_path && (
+            <img
+              src={movies[selectedMovieIndex].poster_path}
+              alt="Movie Poster"
+              style={{ maxHeight: '500px', width: 'auto' }}
+            />
+          )}
           <div className="navigation-buttons">
             <button onClick={handlePrevMovie} disabled={selectedMovieIndex === 0}>
               Previous
