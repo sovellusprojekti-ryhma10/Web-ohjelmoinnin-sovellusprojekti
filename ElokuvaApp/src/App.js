bbc260623717079667623c025d4620f5037d1e01
KaiSuomi-hub <69754493+KaiSuomi-hub@users.noreply.github.com>
2024-04-08 16:37:51 +0300

    navbar starting to get done
    basic function exists, now to style and codethe fron to login and signup

--- ElokuvaApp/src/App.js
+++ ElokuvaApp/src/App.js
@@ -1,21 +1,32 @@
 import React, { useState } from 'react';
 import './App.css';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound'
+
+import Home from "./pages/Home";
+import Navbar from './components/Navbar';
+import UserProvider from './context/UserProvider'
 import { Route, Routes } from 'react-router-dom'
 
-import MovieSearch from './components/MovieSearch';
-import MovieSearchByActorName from './components/MovieSearchByActorName';
-import Navbar from './components/Navbar';
 
 function App() {
   return (
+    <UserProvider>
+       <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+
     <div className="App">
+
       <Navbar />
       <div className="content">
-        {/* Sijoitelkaa sisältö content tagin sisään tähän diviin */}
-      <MovieSearch />
+        {/* Sijoitelkaa sisältö content tagin sisään jokaiselle sivulle ja tuokaa import '../index.css'; sivulle */}
       </div>
     </div>
-  );
+    </UserProvider>
+      );
 }
 
 export default App;
--- ElokuvaApp/src/pages/Login.js
+++ ElokuvaApp/src/pages/Login.js
@@ -1,43 +1,36 @@
-import { useNavigate } from "react-router-dom";
-import "./Login.css";
-import React, { useState } from "react";
-import { useUser } from "../context/useUser";
-
+import { useNavigate } from 'react-router-dom'
+import './Login.css'
+import React, { useState } from 'react'
+import { useUser } from '../context/useUser'
+import "../index.css";
 export default function Login() {
-    const { login } = useUser();
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const navigate = useNavigate();
+  const { login } = useUser()
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const navigate = useNavigate()
 
-    const validate = (e) => {
-        e.preventDefault();
-        if (username.length > 0 && password.length > 0) {
-            const data = { user: username, password: password };
-            login(data);
-        }
-    };
+  const validate = (e) => {
+    e.preventDefault()
+    if (username.length >0 && password.length >0) {
+      const data = {"user":username,"password":password}
+      login(data)
+    }
+  }
 
-    return (
-        <div id="login-form">
-            <form onSubmit={validate}>
-                <h3>Login</h3>
-                <div>
-                    <label>User</label>
-                    <input
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                    />
-                </div>
-                <div>
-                    <label>Password</label>
-                    <input
-                        type="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                </div>
-                <button>Submit</button>
-            </form>
+  return (
+    <div className="content">
+      <form onSubmit={validate}>
+        <h3>Login</h3>
+        <div>
+          <label>User</label>
+          <input value={username} onChange={e => setUsername(e.target.value)}/>
         </div>
-    );
+        <div>
+          <label>Password</label>
+          <input type="password" value={password} onChange={e => setPassword(e.target.value)}/>
+        </div>
+        <button>Submit</button>
+      </form>
+    </div>
+  )
 }
